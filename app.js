22049fb20b98ea6d45033e5071b6dec8fec645b9
James Byrum <me@jimmybyrum.com>
2015-02-22 06:24:30 -0800

    ctrl-s

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
-  var windowWidth = $(window).width();
-  var windowHeight = $(window).height();
-  $('section').css('min-height', windowHeight);
+  var $window = $(window);
+
+  $('section h1').each(function(idx) {
+    var height = $(this).innerHeight();
+    $(this).parents('.row').css('min-height', height + 'px');
+  });
+
+  $window.on('scroll', function() {
+    var scrollTop = $window.scrollTop();
+
+    $('section').each(function() {
+      var thisTop = $(this).offset().top;
+      var thisBottom = thisTop + $(this).innerHeight();
+      if (thisTop < scrollTop) {
+        $(this).addClass('active');
+        $(this).find('h1').addClass('affix');
+      }
+      if (thisTop > scrollTop) {
+        $(this).find('h1').removeClass('affix');
+      }
+      if (thisBottom < scrollTop) {
+        $(this).removeClass('active');
+      }
+    });
+  });
 });
