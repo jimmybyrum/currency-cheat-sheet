1f4c093036065a6f95569ec1c4c96d8f4da9a3df
James Byrum <me@jimmybyrum.com>
2015-02-28 08:55:37 -0800

    scroll

--- app.js
+++ app.js
@@ -3,43 +3,52 @@
   var $html = $('html');
   var $window = $(window);
   var $header = $('header');
-  var $icons = $('.icons');
-  var $title = $('header .title');
-  var $nav = $('nav');
   var $sections = $('section');
 
-  var iconHeight = $icons.innerHeight();
-  var navHeight = $nav.innerHeight();
-  var openHeight = $header.innerHeight();
+  var headerHeight = $header.innerHeight();
 
-  $nav.css('height', navHeight);
-  $header.css('position', 'fixed');
-  $('body').css('padding-top', openHeight + 'px');
+  var $scrollNav = $header.clone();
+  $scrollNav.find('.icons').remove();
+  $scrollNav.addClass('scroll-nav');
+  $header.find('.bookmark').remove();
+  $scrollNav.appendTo('body');
+  var scrollNavHeight = $scrollNav.innerHeight();
 
-  function openNav() {
-    $nav.css('height', navHeight);
-  }
+  var closed = false;
   function closeNav() {
-    $nav.css('height', 0);
+    $scrollNav.css({
+      top: -(scrollNavHeight) + 'px'
+    });
+    closed = true;
   }
+  function openNav() {
+    $scrollNav.css({
+      top: 0
+    });
+    closed = false;
+  }
+  closeNav();
 
-  $('.bookmark').on('click', function(e) {
+  $scrollNav.find('.bookmark').on('click', function(e) {
     e.preventDefault();
-    if (parseInt($nav.css('height'), 10) === 0) {
+    if (closed) {
       openNav();
     } else {
       closeNav();
     }
   });
 
-  $nav.find('a').on('click', function(e) {
+  $('nav a, .scroll-nav nav a').on('click', function(e) {
     var target = $(this).attr('href');
     if (target && target !== '') {
       closeNav();
       var $target = $(target);
-      var top = $target.offset().top - 60;
+      var top = $target.offset().top - 30;
       $.scrollTo(top, 300, {easing: 'swing'});
     }
+  });
+  $('.title').on('click', function() {
+    $.scrollTo(0, 300, {easing: 'swing'});
   });
 
   $sections.each(function(idx) {
@@ -49,31 +58,11 @@
 
   function onVisibilityChange() {
     var scrollTop = $window.scrollTop();
-    if (scrollTop > 50) {
+    if (scrollTop > headerHeight) {
       $html.addClass('scrolling');
     } else {
       $html.removeClass('scrolling');
     }
-    if (scrollTop < navHeight) {
-      var newNavHeight = navHeight - scrollTop;
-      $nav.css('height', newNavHeight);
-      var newIconHeight = iconHeight - scrollTop;
-      if (newIconHeight >= 0) {
-        $icons.css('height', newIconHeight);
-      }
-      var fontSize = 80 - (scrollTop / 4);
-      if (fontSize >= 25) {
-        $title.css('font-size', fontSize + 'px');
-        $html.removeClass('mini');
-      } else if (fontSize < 20) {
-        $html.addClass('mini');
-      }
-    } else {
-      $nav.css('height', 0);
-      $icons.css('height', 0);
-      $title.css('font-size', '25px');
-      $html.addClass('mini');
-    }
   }
-  $window.on('resize scroll', onVisibilityChange);
+  $window.on('DOMContentReady load resize scroll', onVisibilityChange);
 })();
