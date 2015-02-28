27bd5199a48526dedd784c5110f964c2c7359c68
James Byrum <me@jimmybyrum.com>
2015-02-28 07:36:54 -0800

    saving with multiple scroll

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
-var ccs = (function() {
+(function() {
   'use strict';
   var $html = $('html');
   var $window = $(window);
+  var $header = $('header');
+  var $icons = $('.icons');
+  var $title = $('header .title');
   var $nav = $('nav');
-  var $title = $('nav a');
   var $sections = $('section');
+
+  var iconHeight = $icons.innerHeight();
+  var navHeight = $nav.innerHeight();
+  var openHeight = $header.innerHeight();
+
+  $nav.css('height', navHeight);
+  $header.css('position', 'fixed');
+  $('body').css('padding-top', openHeight + 'px');
+
+  function openNav() {
+    $nav.css('height', navHeight);
+  }
+  function closeNav() {
+    $nav.css('height', 0);
+  }
+
+  $('.bookmark').on('click', function(e) {
+    e.preventDefault();
+    if (parseInt($nav.css('height'), 10) === 0) {
+      openNav();
+    } else {
+      closeNav();
+    }
+  });
 
   $nav.find('a').on('click', function(e) {
     var target = $(this).attr('href');
     if (target && target !== '') {
+      closeNav();
       var $target = $(target);
-      $.scrollTo($target, 300, {easing: 'swing'});
-    }
-  });
-
-  $nav.css('height', $nav.innerHeight());
-
-  $('.bookmark').on('click', function(e) {
-    e.preventDefault();
-    if ($nav.hasClass('closed')) {
-      $nav.removeClass('closed');
-    } else {
-      $nav.addClass('closed');
+      var top = $target.offset().top - 60;
+      $.scrollTo(top, 300, {easing: 'swing'});
     }
   });
 
@@ -30,35 +47,33 @@ var ccs = (function() {
     $sectionTitle.text((++idx) + '. ' + $sectionTitle.text());
   });
 
-  // function onVisibilityChange() {
-  //   var scrollTop = $window.scrollTop();
-  //   var windowHeight = window.innerHeight || document.documentElement.clientHeight;
-  //   var scrollBottom = scrollTop + windowHeight;
-
-  //   if (scrollTop > $('.lander').innerHeight()) {
-  //     $html.addClass('scrolling');
-  //   } else {
-  //     $html.removeClass('scrolling');
-  //   }
-
-  //   $sections.each(function(idx) {
-  //     var $section = $(this);
-  //     var title = $section.find('h2').text();
-
-  //     var rect = $section[0].getBoundingClientRect();
-
-  //     var pastTop = rect.top <= 0;
-  //     var beforeBottom = rect.bottom >= 0 && rect.bottom < scrollBottom;
-
-  //     var inViewport = pastTop && beforeBottom;
-
-  //     if (inViewport && title) {
-  //       $title.text(title);
-  //       $section.addClass('active');
-  //     } else {
-  //       $section.removeClass('active');
-  //     }
-  //   });
-  // }
-  // $window.on('DOMContentLoaded load resize scroll', onVisibilityChange);
+  function onVisibilityChange() {
+    var scrollTop = $window.scrollTop();
+    if (scrollTop > 50) {
+      $html.addClass('scrolling');
+    } else {
+      $html.removeClass('scrolling');
+    }
+    if (scrollTop < navHeight) {
+      var newNavHeight = navHeight - scrollTop;
+      $nav.css('height', newNavHeight);
+      var newIconHeight = iconHeight - scrollTop;
+      if (newIconHeight >= 0) {
+        $icons.css('height', newIconHeight);
+      }
+      var fontSize = 80 - (scrollTop / 4);
+      if (fontSize >= 25) {
+        $title.css('font-size', fontSize + 'px');
+        $html.removeClass('mini');
+      } else if (fontSize < 20) {
+        $html.addClass('mini');
+      }
+    } else {
+      $nav.css('height', 0);
+      $icons.css('height', 0);
+      $title.css('font-size', '25px');
+      $html.addClass('mini');
+    }
+  }
+  $window.on('resize scroll', onVisibilityChange);
 })();
